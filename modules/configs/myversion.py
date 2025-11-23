bfd7721b214490de37ce836715f5666ae49d498f
Yusen Wu <61451492+sanmusen214@users.noreply.github.com>
2025-11-23 13:18:23 +0800

    Bump version to 1.11.8 and update changelog
    Updated version to 1.11.8 and modified changelog entries.

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -1,34 +1,8 @@
-myversion = "1.11.7"
+myversion = "1.11.8"
 mychangelog = """
 
 ## Fix
 
-修复国服反和谐后会卡在登录社区弹窗三十秒的问题
-
-修复总力战助战选择学生失败的问题
-
-Fix Assault can not select help student
-
-## Feat
-
-（由于是实验性功能，有问题请到q群反馈985729020）
-
-新增GUI操作流实现 更易上手，更高自由度，更稳定 的开启加速器和自定义任务流程
-
-More flexible user define tasks
-
-考试也可以选择助战学生了
-
-Can select helper student in Exam
-
-## Update
-
-以前的开启加速器流程迁移至新的操作流。按理说自动迁移应该没什么问题，建议各位sensei更新后观察下开启和关闭加速器流程是否正常
-
-## Build
-
-使用github自动构建并发布release
-
-Auto-release in github action
+在 GitHub actions 里使用 miniconda 打包以解决包不兼容问题
 
 """
